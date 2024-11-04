fee0d61c8fe40a89c724761df4150d607383cd1a
Sivapriya J Sivan <sivapriyajsivan@gmail.com>
2024-11-04 22:46:18 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,13 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Sivapriya J Sivan.<br> </span> An enthusiastic web development fresher with practical experience in HTML, CSS, JavaScript, and frameworks like Bootstrap, I'm passionate about building dynamic, user-focused digital experiences. Blending creativity with technical skill, I am dedicated to delivering seamless interactions and polished designs. With a strong commitment to continuous learning and a drive to innovate, I look forward to contributing fresh perspectives and growing my expertise through hands-on, real-world projects."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
+
+  
+
+  
